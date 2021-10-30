25acb0e9c1a82cf680f818dd125f3454dfd8e348
rawan <missrero99@gmail.com>
2021-10-30 12:31:01 +0200

    modified login screen and sign up screen

--- app/src/main/java/com/example/bmi/LoginScreen.java
+++ app/src/main/java/com/example/bmi/LoginScreen.java
@@ -2,14 +2,26 @@ package com.example.bmi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginScreen extends AppCompatActivity {
-
+TextView signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
         getSupportActionBar().setTitle("BMI Analyzer");
+        signup=findViewById(R.id.textView5);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(LoginScreen.this,SignUpScreen.class);
+                startActivity(i);
+
+            }
+        });
     }
 }
