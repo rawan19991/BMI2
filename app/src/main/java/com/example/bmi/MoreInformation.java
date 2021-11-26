dbee6aa420538ca9e85644465c43583953e878d4
rawan <missrero99@gmail.com>
2021-11-26 14:08:07 +0200

    5 26-11

--- app/src/main/java/com/example/bmi/MoreInformation.java
+++ app/src/main/java/com/example/bmi/MoreInformation.java
@@ -2,13 +2,24 @@ package com.example.bmi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MoreInformation extends AppCompatActivity {
-
+Button save_data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_more_information);
+        save_data=findViewById(R.id.button7);
+        save_data.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MoreInformation.this,new_record.class);
+                startActivity(i);
+            }
+        });
     }
 }
